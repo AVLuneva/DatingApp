cf95e633609d88361e3bff8fd1560ecb47811cbd
Alexandra Luneva <Luneva.AV@mail.ru>
2022-01-17 15:03:22 +0700

    Create AccountController

--- src/WebUI/Controllers/UsersController.cs
+++ src/WebUI/Controllers/UsersController.cs
@@ -9,9 +9,7 @@ using WebUI.Entities;
 
 namespace WebUI.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+    public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
         public UsersController(DataContext context)

aca535cef6a989a1a0d11ba196756b7fb8b144e2
Alexandra Luneva <Luneva.AV@mail.ru>
2022-01-18 10:36:46 +0700

    Add Login EndPoint

--- src/WebUI/Controllers/AccountController.cs
+++ src/WebUI/Controllers/AccountController.cs
@@ -40,6 +40,26 @@ namespace WebUI.Controllers
             return user;
         }
 
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDTO loginDto)
+        {
+            var user = await _context.Users
+                .SingleOrDefaultAsync(x => x.UserName == loginDto.UserName);
+            
+            if (user == null) return Unauthorized("Invalid username");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computeHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i = 0; i < computeHash.Length; i++)
+            {
+                if (computeHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+            }
+
+            return user;
+        }
+
         private async Task<bool> UserExists(string userName)
         {
             return await _context.Users.AnyAsync(x => x.UserName == userName.ToLower());

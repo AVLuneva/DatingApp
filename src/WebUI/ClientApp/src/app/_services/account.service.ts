c440a6a7435ce778496bd619175dea986ff4792c
Alexandra Luneva <Luneva.AV@mail.ru>
2022-01-21 09:10:21 +0700

    Add currentUser

--- src/WebUI/ClientApp/src/app/_services/account.service.ts
+++ src/WebUI/ClientApp/src/app/_services/account.service.ts
@@ -1,15 +1,37 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ReplaySubject } from 'rxjs';
+import {map} from 'rxjs/operators';
+import { User } from '../_models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
-  baseUrl = 'https://localhost:5001/api/'
+  baseUrl = 'https://localhost:5001/api/';
+  private currenUserService = new ReplaySubject<User>(1);
+  currentUser$ = this.currenUserService.asObservable();
 
   constructor(private http: HttpClient) { }
 
   login(model: any) {
-    return this.http.post(this.baseUrl + 'account/login', model)
+    return this.http.post(this.baseUrl + 'account/login', model).pipe(
+      map((response: User) => {
+        const user = response;
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currenUserService.next(user);
+        }
+      })
+    )
+  }
+
+  setCurrentUser(user: User) {
+    this.currenUserService.next(user);
+  }
+
+  logout(){
+    localStorage.removeItem('user');
+    this.currenUserService.next(null);
   }
 }

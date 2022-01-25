c1e2dc77c6a521955522525265b8ba58935db14a
Alexandra Luneva <Luneva.AV@mail.ru>
2022-01-25 19:32:17 +0700

    Add errors pages

--- src/WebUI/Startup.cs
+++ src/WebUI/Startup.cs
@@ -35,7 +35,6 @@ namespace WebUI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddAplicationServices(Configuration);
             services.AddControllers();
             services.AddCors();

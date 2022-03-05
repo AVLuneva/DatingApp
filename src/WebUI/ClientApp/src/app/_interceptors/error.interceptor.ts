9f0fd35ef8ab778aec859f27b0916a60cfa65abf
Alexandra Luneva <Luneva.AV@mail.ru>
2022-03-05 08:57:35 +0700

    Add like component to client

--- src/WebUI/ClientApp/src/app/_interceptors/error.interceptor.ts
+++ src/WebUI/ClientApp/src/app/_interceptors/error.interceptor.ts
@@ -13,14 +13,14 @@ import { catchError } from 'rxjs/operators';
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-  constructor(private router: Router, private toastr: ToastrService) {}
+  constructor(private router: Router, private toastr: ToastrService) { }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     return next.handle(request).pipe(
       catchError(error => {
         if (error) {
           switch (error.status) {
-            case 400: 
+            case 400:
               if (error.error.errors) {
                 const modelStateErrors = [];
                 for (const key in error.error.errors) {
@@ -29,18 +29,20 @@ export class ErrorInterceptor implements HttpInterceptor {
                   }
                 }
                 throw modelStateErrors.flat();
+              } else if (typeof (error.error === 'object')) {
+                this.toastr.error(error.error, error.status);
               } else {
-                this.toastr.error(error.statusText, error.status);
+                this.toastr.error(error.error, error.status);
               }
               break;
             case 401:
-              this.toastr.error(error.statusText, error.status);
+              this.toastr.error(error.error, error.status);
               break;
             case 404:
               this.router.navigateByUrl('/not-found');
               break;
             case 500:
-              const navigationExtras: NavigationExtras = {state: {error: error.error}}
+              const navigationExtras: NavigationExtras = { state: { error: error.error } }
               this.router.navigateByUrl('/server-error', navigationExtras);
               break;
             default:

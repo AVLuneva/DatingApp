8b980657c862720f667786ca058cebede79a6dc8
Alexandra Luneva <Luneva.AV@mail.ru>
2022-03-05 17:31:34 +0700

    Create message repository

---
+++ src/WebUI/Interfaces/IMessageRepository.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebUI.DTOs;
+using WebUI.Entities;
+using WebUI.Helpers;
+
+namespace WebUI.Interfaces
+{
+    public interface IMessageRepository
+    {
+        void AddMessage(Message message);
+        void DeleteMessage(Message message);
+        Task<Message> GetMessage(int id);
+        Task<PagedList<MessageDto>> GetMessagesForUser();
+        Task<IEnumerable<MessageDto>> GetMessageThread(int currentUserId, int recipientId);
+        Task<bool> SaveAllAsync();
+    }
+}

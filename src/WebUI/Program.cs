79e0347a223d2a18408d5a00115b02aa37188f8f
Alexandra Luneva <Luneva.AV@mail.ru>
2022-03-10 08:22:51 +0700

    Add migration for Identity

--- src/WebUI/Program.cs
+++ src/WebUI/Program.cs
@@ -1,12 +1,9 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using WebUI.Data;
 
@@ -24,7 +21,7 @@ namespace WebUI
             {
                 var context = services.GetRequiredService<DataContext>();
                 await context.Database.MigrateAsync();
-                await Seed.SeedUsers(context);
+                //await Seed.SeedUsers(context);
             }
             catch (Exception ex)
             {
